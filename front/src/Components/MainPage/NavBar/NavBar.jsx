bf00c1c889abcf287d175bdae761d81667d8afa7
mmaneel <m.mohandouali@esi-sba.dz>
2024-03-13 01:39:52 +0100

    subscribe page

--- front/src/Components/MainPage/NavBar/NavBar.jsx
+++ front/src/Components/MainPage/NavBar/NavBar.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
-import './NavBar.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 function NavBar() {
-
   return (
-    <>
     <div className='navBar_container'>
-        <div className='dv_btn'>
-            <button className='navbar_btn'>! اشترك معنا الان</button>
-        </div>
-        <div >
-            <ul className='NavBar_item'>
-                <div className='li_item1'><li>حول المنصة </li></div>
-                <div className='li_item1'><li>عروضنا</li></div>
-                <div className='li_item1'><li>المصادر الرسمية</li></div>
-                <div className='li_item1'><li>اخر المستجدات</li></div>
-                <div className='li_item1'><li>الرئيسية</li></div>
-            </ul>
-            
-        </div>
+      <div className='dv_btn'>
+        <Link to="/subscrib" className='navbar_btn'>! اشترك معنا الان</Link>
+      </div>
+      <div >
+        <ul className='NavBar_item'>
+          <div className='li_item1'><Link to="/about-us"><li>حول المنصة </li></Link></div>
+          <div className='li_item1'><Link to="/subscrib"><li>عروضنا</li></Link></div>
+          <div className='li_item1'><Link to="/official-resources"><li>المصادر الرسمية</li></Link></div>
+          <div className='li_item1'><Link to="/latest-news"><li>اخر المستجدات</li></Link></div>
+          <div className='li_item1'><Link to="/home"><li>الرئيسية</li></Link></div>
+        </ul>
+      </div>
     </div>
-    </>
-   
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
