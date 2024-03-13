6addd3f06543f2cffcb5dbd4c33b8c5fd9a9ce7c
mmaneel <m.mohandouali@esi-sba.dz>
2024-03-13 01:31:55 +0100

    subscribe page

--- front/src/Components/Subscrib/Subscrib.jsx
+++ front/src/Components/Subscrib/Subscrib.jsx
@@ -5,8 +5,38 @@ import "./Subscrib.css"
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Subscrib() {
+    const offers = [
+        {
+          title: 'العرض الثالث',
+          price: '1000دج/شهر',
+          features: [
+            'الاطلاع على كل النصوص القانونية',
+            'الحصول على كل المستجدات',
+            'تلقي تنبيهات متعلقة بالمستجدات'
+          ]
+        },
+        {
+          title: 'العرض الثاني',
+          price: '1000دج/شهر',
+          features: [
+            'الاطلاع على كل النصوص القانونية',
+            'الحصول على كل المستجدات',
+            'تلقي تنبيهات متعلقة بالمستجدات'
+          ]
+        },
+        {
+          title: 'العرض الأول',
+          price: '1000دج/شهر',
+          features: [
+            'الاطلاع على كل النصوص القانونية',
+            'الحصول على كل المستجدات',
+            'تلقي تنبيهات متعلقة بالمستجدات'
+          ]
+        }
+      ];
+
   return (
-    <>
+   <>
     <Logo/>
     <div className='sub_titre'>
       <h2>  عروض الاشتراك</h2>
@@ -14,55 +44,24 @@ function Subscrib() {
     <div className='sub_container'>
         <p className='offre_title'>إختاروا العرض الذي يناسبكم</p>
         <div className='sub_offers'>
-            <div className='offre'>
-            <p className='offre_title'> العرض الثالث</p>
-            <h3 >1000دج/شهر</h3>
-            <img  className="offer_icon " src="./images/echelledejusticeB.png"/>
-            <div >
-                <p className='list_offers'>: ميزات العرض</p>
-                <ul>
-                   <li>الاطلاع على كل النصوص القانونية</li>
-                   <li>الحصول على كل المستجدات</li>
-                   <li>تلقي تنبيهات متعلقة بالمستجدات</li>
-                </ul>
-            </div>
-            
-            <button className='btn_sub'>
-                اشتراك<ShoppingCartIcon sx={{ width: '20px', height: '20px',marginLeft:'5px' }}/>
-            </button>
-            </div>
-            <div className='offre'>
-            <p className='offre_title'>  العرض الثاني</p>
-            <h3 >1000دج/شهر</h3>
-            <img className="offer_icon "  src="./images/echelledejusticeB.png"/>
-            <div >
-                <p className='list_offers'>: ميزات العرض</p>
-                <ul>
-                   <li>الاطلاع على كل النصوص القانونية</li>
-                   <li>الحصول على كل المستجدات</li>
-                   <li>تلقي تنبيهات متعلقة بالمستجدات</li>
-                </ul>
-            </div>
-            <button className='btn_sub'>
-                اشتراك<ShoppingCartIcon sx={{ width: '20px', height: '20px',marginLeft:'5px' }}/>
-            </button>
-            </div>
-            <div className='offre'>
-            <p className='offre_title'>  العرض الأول</p>
-            <h3 >1000دج/شهر</h3>
-            <img  className="offer_icon "  src="./images/echelledejusticeB.png"/>
-            <div >
-                <p className='list_offers'>: ميزات العرض</p>
-                <ul>
-                   <li>الاطلاع على كل النصوص القانونية</li>
-                   <li>الحصول على كل المستجدات</li>
-                   <li>تلقي تنبيهات متعلقة بالمستجدات</li>
-                </ul>
-            </div>
-            <button className='btn_sub'>
-                اشتراك<ShoppingCartIcon sx={{ width: '20px', height: '20px',marginLeft:'5px' }}/>
-            </button>
-            </div>
+            {offers.map((offer, index) => (
+              <div className='offre' key={index}>
+                <p className='offre_title'>{offer.title}</p>
+                <h3>{offer.price}</h3>
+                <img className="offer_icon" src="./images/echelledejusticeB.png"/>
+                <div>
+                    <p className='list_offers'>: ميزات العرض</p>
+                    <ul>
+                      {offer.features.map((feature, index) => (
+                        <li key={index}>{feature}</li>
+                      ))}
+                    </ul>
+                </div>
+                <button className='btn_sub'>
+                    اشتراك<ShoppingCartIcon sx={{ width: '20px', height: '20px',marginLeft:'5px' }}/>
+                </button>
+              </div>
+            ))}
         </div>
     </div>
     <Footer/>
