6d1669f4b72e5a06adbecb74d610bf5859dac8cf
mmaneel <m.mohandouali@esi-sba.dz>
2024-03-31 16:46:29 +0200

    scraping page done

--- front/src/Components/Admin/Scraping/Scraping.jsx
+++ front/src/Components/Admin/Scraping/Scraping.jsx
@@ -3,35 +3,41 @@ import "./Scraping.css";
 import LogoAdmin from '../../LOGO/LogoAdmin';
 import TitleBar from '../../TitleBar/TitleBar';
 import FooterAdmin from '../../Footer/FooterAdmin';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function Scraping() {
-  // State pour stocker l'option sélectionnée
-  const [selectedOption, setSelectedOption] = useState("periodically");
+  
+  const [selectedOption, setSelectedOption] = useState("periodically"); //stocker l'option sélectionnée
+  const [frequency, setFrequency] = useState(""); // la fréquence sélectionnée
+  const [customDate, setCustomDate] = useState("");// la date personnalisée
+  const [selectedUrls, setSelectedUrls] = useState([]);// stocker les URLs sélectionnées
+  const [showOptions, setShowOptions] = useState(false);// l'affichage des options
+  const [legalText, setLegalText] = useState("");
 
-  // State pour stocker la fréquence sélectionnée
-  const [frequency, setFrequency] = useState("");
-
-  // State pour stocker la date personnalisée
-  const [customDate, setCustomDate] = useState("");
-
-  // Gestionnaire d'événements pour le changement d'option
+const handleLegalTextChange = (event) => {
+    setLegalText(event.target.value);
+  };
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
+    setFrequency("");
+  setCustomDate("");
+ 
   };
-
-  // Gestionnaire d'événements pour la sélection de la fréquence
   const handleFrequencyChange = (event) => {
     setFrequency(event.target.value);
   };
-
-  // Gestionnaire d'événements pour le changement de la date personnalisée
   const handleCustomDateChange = (event) => {
     setCustomDate(event.target.value);
   };
-
-  // Gestionnaire d'événements pour le bouton "Submit"
-  const handleSubmit = () => {
-    // Envoyer les informations au backend ou effectuer d'autres actions
+  const handleUrlChange = (event) => {
+    const value = event.target.value;
+    if (event.target.checked) {
+      setSelectedUrls([...selectedUrls, value]);
+    } else {
+      setSelectedUrls(selectedUrls.filter(url => url !== value));
+    }
+  };
+  const handleSaveInformation = () => {
     console.log("Selected Option:", selectedOption);
     if (selectedOption === "periodically") {
       console.log("Frequency:", frequency);
@@ -39,7 +45,22 @@ function Scraping() {
       console.log("Custom Date:", customDate);
     }
   };
-
+  const handleStartScraping = () => {
+    const scrapingData = {
+        selectedOption: selectedOption,
+        frequency: frequency,
+        customDate: customDate,
+        selectedUrls: selectedUrls,
+        legalText:legalText
+      };
+    // Envoyer les informations au backend
+    console.log(scrapingData);
+   
+  };
+ const handleDisplayOptions = () => {
+    setShowOptions(!showOptions);
+  };
+   
   return (
     <>
       <LogoAdmin title="صفحة الاشراف "/>
@@ -60,7 +81,6 @@ function Scraping() {
                           onChange={handleOptionChange}
                         />
                       </label>
-                     
                    </div>
                    <div className='scraping-frequency'>
                      <ul>
@@ -128,17 +148,88 @@ function Scraping() {
                      />
                    </div>
                    <div className='btn-dv-scraping'>
-                     <button className='btn-date' onClick={handleSubmit}>حفظ المعلومات </button>
+                     <button className='btn-date' onClick={handleSaveInformation}>حفظ المعلومات </button>
                    </div>
               </div>
               <div className='scraping-content'>
                   <h4>ملأ المعلومات الأساسية</h4>
-                  <div>
-                    
+                  <div className='scraping form'>
+                    <form >
+                      <div className='scraping-info'>
+                      <input
+                          type="text"
+                          id="textjuridique"
+                          name="textjuridique"
+                          placeholder=' النص القانوني ....'
+                          className='scraping-info-item'
+                          value={legalText}
+                          onChange={handleLegalTextChange}
+                          required
+                        />
+                      </div>
+                      <div className='scraping-info'>
+                        
+                        <div className="display-options" >
+                        <div className='titre-url'>
+                          <p>حدد الروابط </p> 
+                        </div>
+                          <ExpandMoreIcon  onClick={handleDisplayOptions} />
+                          <i className="fa fa-chevron-down"></i>
+                        </div>
+                        <div className="checkbox-options" style={{ display: showOptions ? 'block' : 'none' }}>
+                          <div>
+                            <input
+                              type="checkbox"
+                              name="url1"
+                              value="https://www.futura-sciences.com."
+                              onChange={handleUrlChange}
+                            />
+                            https://www.futura-sciences.com.
+                          </div>
+                          <div>
+                            <input
+                              type="checkbox"
+                              name="url2"
+                              value="https://www.futura-sciences.com."
+                              onChange={handleUrlChange}
+                            />
+                            https://www.futura-sciences.com.
+                          </div>
+                          <div>
+                            <input
+                              type="checkbox"
+                              name="url3"
+                              value="https://www.futura-sciences.com."
+                              onChange={handleUrlChange}
+                            />
+                            https://www.futura-sciences.com.
+                          </div>
+                          <div>
+                            <input
+                              type="checkbox"
+                              name="url4"
+                              value="https://www.futura-sciences.com."
+                              onChange={handleUrlChange}
+                            />
+                            https://www.futura-sciences.com.
+                          </div>
+                          <div>
+                            <input
+                              type="checkbox"
+                              name="url5"
+                              value="https://www.futura-sciences.com."
+                              onChange={handleUrlChange}
+                            />
+                            https://www.futura-sciences.com.
+                          </div>
+                        </div>
+                        
+                      </div>
+                    </form>
                   </div>
               </div>
           </div>
-          <button className='btn-scraping'>بدأ عملية الكشط و البحث </button>
+          <button className='btn-scraping' onClick={handleStartScraping}>بدأ عملية الكشط و البحث </button>
       </div>
       <FooterAdmin/>
     </>
